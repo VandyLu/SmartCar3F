6682f3c8fad04c75528f4048d751262ee95c7c02
VandyLu <lfb97820@sjtu.edu.cn>
2018-04-02 23:03:33 +0800

    a simple framework for 3f

---
+++ picar.py
@@ -0,0 +1,17 @@
+
+import smartcarlib
+import smartcarlib.cv
+
+class Picar():
+
+    def __init__():
+        pass
+
+    def cruise():
+        pass
+
+    def park():
+        pass
+
+if __name__ == '__main__':
+    print smartcarlib.cv
---
+++ smartcarlib/cv.py
@@ -0,0 +1,8 @@
+# This computer vision part of smartcar library
+# Author: LFB
+
+import cv2
+import numpy as np
+
+def line_segment(image, threshold=1.0):
+    pass
