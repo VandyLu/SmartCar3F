8f3d50746ea8cab75de7dde94a2230bef6fa680f
VandyLu <lfb97820@sjtu.edu.cn>
2018-04-02 23:27:32 +0800

    add gitignore

--- picar.py
+++ picar.py
@@ -1,17 +1,50 @@
 
 import smartcarlib
 import smartcarlib.cv
+import smartcarlib.driver
+import smartcarlib.PID
+
+import cv2
+import collections.namedtuple
+
+PicarParams = namedtuple('PicarParams',
+                         ['threshold'])
 
 class Picar():
 
-    def __init__():
+    def __init__(self, params):
+        self.params = params
+
+        self.front = 0
+        self.back = 1
+        self.cap = [cv2.VideoCapture(i) for i in range(2)]
+        self.driver = smartcarlib.driver.driver()
+
+    def cruise(self):
+        ''' Cruise black line
+        '''
+        
+        image = self.query_camera(self.front)
+
+        # Segment the black line to points list nx2
+        points = smartcarlib.cv.line_segment(image, self.params.threshold)
+
+        # Get target 
+
+
+    def park(self):
+        ''' Park to specified plot
+        '''
         pass
 
-    def cruise():
-        pass
+    def query_camera(self, cam_id):
+        ''' Get a frame from front/back camera
+        '''
+        assert cam_id in (0, 1)
 
-    def park():
-        pass
+        ret, frame = self.cap[cam_id]
+        return frame
+
 
 if __name__ == '__main__':
     print smartcarlib.cv
---
+++ smartcarlib/PID.py
@@ -0,0 +1,19 @@
+
+class PID():
+    ''' A naive PID implementation '''
+
+    def __init__(self, kp, ki, kd):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+
+        self.int_e = 0.0
+        self.last_e = 0.0
+
+    def update(err):
+        output = self.kp * err + self.ki * self.int_e + self.kd * (err - self.last_e)
+        # Update integral and derivative
+        self.int_e += err
+        self.last_e = err
+
+        return output
