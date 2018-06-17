534d75dceacfecc6c4502d9af80bbea5beaff62c
VandyLu <lfb97820@sjtu.edu.cn>
2018-06-17 12:43:37 +0800

    6.17

---
+++ smartcarlib/parklot.py
@@ -0,0 +1,24 @@
+
+import cv2
+import numpy as np
+
+def detect_parklot(image, params):
+    ''' WZK
+        
+        return the direction of the parking lot and it's position
+    '''
+
+    return 
+
+
+def test_parklot():
+    ''' A test function
+        Reading an image and run your code, see the outputs
+    '''
+
+    image = cv2.imread('', -1)
+
+    detect_parklot(image, None)
+
+    return
+
